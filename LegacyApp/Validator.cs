30a81d80becee1465930f96e7656e5d4a3451fe3
piszczel24 <s26479@pjwstk.edu.pl>
2024-04-05 04:18:12 +0200

    Dodano walidator

---
+++ LegacyApp/Validator.cs
@@ -0,0 +1,14 @@
+﻿namespace LegacyApp;
+
+public static class Validator
+{
+    public static bool IsFirstNameAndLastNameValid(string firstName, string lastName)
+    {
+        return !string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName);
+    }
+
+    public static bool IsEmailAddressValid(string email)
+    {
+        return email.Contains('@') || email.Contains('.');
+    }
+}
