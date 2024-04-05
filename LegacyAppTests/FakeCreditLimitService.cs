c51c7d6533bc54caed188b6f8c9ffa0da95624fb
piszczel24 <s26479@pjwstk.edu.pl>
2024-04-05 04:18:38 +0200

    Dodano parę testów

---
+++ LegacyAppTests/FakeCreditLimitService.cs
@@ -0,0 +1,14 @@
+﻿using LegacyApp;
+
+namespace LegacyAppTests;
+
+public class FakeCreditLimitService : ICreditLimitService
+{
+    private static readonly int[] CreditLimits = [200, 20000, 10000, 3000, 1000];
+    private static readonly Random Random = new();
+
+    public int GetCreditLimit(string lastName, DateTime dateOfBirth)
+    {
+        return CreditLimits[Random.Next(CreditLimits.Length)];
+    }
+}
--- LegacyAppTests/UserServiceTests.cs
+++ LegacyAppTests/UserServiceTests.cs
@@ -4,6 +4,9 @@ namespace LegacyAppTests;
 
 public class UserServiceTests
 {
+    private static readonly FakeClientRepository FakeClientRepository = new();
+    private static readonly FakeCreditLimitService FakeCreditLimitService = new();
+
     [Fact]
     public void AddUser_Should_Return_False_When_Email_Without_At_And_Dot()
     {
@@ -13,7 +16,43 @@ public class UserServiceTests
         var birthDate = new DateTime(1980, 1, 1);
         const int clientId = 1;
         const string email = "johndoegmailcom";
-        var userService = new UserService();
+        var userService = new UserService(FakeClientRepository, FakeCreditLimitService);
+
+        // Act
+        var result = userService.AddUser(firstName, lastName, email, birthDate, clientId);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void AddUser_Should_Return_False_When_FirstName_Is_Empty()
+    {
+        // Arrange
+        const string firstName = "";
+        const string lastName = "Doe";
+        var birthDate = new DateTime(1980, 1, 1);
+        const int clientId = 1;
+        const string email = "johndoegmailcom";
+        var userService = new UserService(FakeClientRepository, FakeCreditLimitService);
+
+        // Act
+        var result = userService.AddUser(firstName, lastName, email, birthDate, clientId);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void AddUser_Should_Return_False_When_LastName_Is_Empty()
+    {
+        // Arrange
+        const string firstName = "John";
+        const string lastName = "";
+        var birthDate = new DateTime(1980, 1, 1);
+        const int clientId = 1;
+        const string email = "johndoegmailcom";
+        var userService = new UserService(FakeClientRepository, FakeCreditLimitService);
 
         // Act
         var result = userService.AddUser(firstName, lastName, email, birthDate, clientId);
